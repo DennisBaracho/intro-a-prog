b381c3b5a9f8f96e4da00ad06020bdf90511a319
Dennis Baracho <97906600+DennisBaracho@users.noreply.github.com>
2024-12-11 13:49:06 -0300

    Update ex013.py

--- lista3/ex013.py
+++ lista3/ex013.py
@@ -14,11 +14,11 @@ print(f"Para A, o valor de S eh: {s}")
 
 s = 0
 expoente = 1
-numerador = 2**expoente
+numerador = 2
 denominador = 50
 
 while(expoente != 50):
-    s += numerador/denominador
+    s += (numerador**expoente)/denominador
     expoente += 1
     denominador -= 1
 print(f"Para B, o valor de S eh: {s}")
